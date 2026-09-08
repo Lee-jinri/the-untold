bf494a5a1b61151e84675045c1f792a33fdabce5
Lee Jinri <ar971004@naver.com>
2026-09-09 02:41:10 +0900

    chore: SessionKeywordRepository 중복 메서드 정리

--- untold-backend/src/main/java/com/untold/backend/session/SessionKeywordRepository.java
+++ untold-backend/src/main/java/com/untold/backend/session/SessionKeywordRepository.java
@@ -6,8 +6,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SessionKeywordRepository extends JpaRepository<SessionKeyword, UUID>{
-	List<SessionKeyword> findByGameSession_id(UUID sessionId);
-	boolean existsByGameSession_idAndKeyword_id(UUID sessionId, UUID keywordId);
+	boolean existsByGameSession_IdAndKeyword_Id(UUID sessionId, UUID keywordId);
 	void deleteByGameSession_GameCase_Id(UUID id);
 	List<SessionKeyword> findByGameSession_Id(UUID sessionId);
 }
